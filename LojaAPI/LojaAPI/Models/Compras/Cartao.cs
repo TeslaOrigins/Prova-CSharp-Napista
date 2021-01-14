9d624333568d41f30435961cfd916298a8c92428
TeslaOrigins <matheuslourenco200@gmail.com>
2021-01-14 10:52:54 -0300

    Adicionado modelos de Cartao e Compra

---
+++ LojaAPI/LojaAPI/Models/Compras/Cartao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaAPI.Models.Compras
+{
+    public class Cartao
+    {
+        [StringLength(60, MinimumLength = 3)]
+        public String Titular { get; set; }
+        [StringLength(19, MinimumLength = 13)]
+        public String Numero { get; set; }
+        [Display(Name = "Data expiracao")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Data_expiracao { get; set; }
+        [StringLength(20, MinimumLength = 3)]
+        public String Bandeira { get; set; }
+        [Range(100, 1000)]
+        [DataType(DataType.Currency)]
+        public int Cvv { get; set; }
+    }
+}
---
+++ LojaAPI/LojaAPI/Models/Compras/Compra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaAPI.Models.Compras
+{
+    public class Compra
+    {
+        public int Valor { get; set; }
+        public Cartao Cartao { get; set; }
+    }
+}
